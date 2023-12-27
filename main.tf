ddd952c8e7c61744823cbf8f5c24205e367dbbfc
Jorg3Garci4 <154987426+Jorg3Garci4@users.noreply.github.com>
2023-12-27 17:33:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,37 @@
-provider "aws" {
-  region = var.region
+provider "google" {
+  credentials = file("./smiling-drake-403912-87d5d6474720.json")
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "google_container_cluster" "default" {
+  name        = var.name
+  project     = var.project
+  description = "Demo GKE Cluster"
+  location    = var.location
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+  enable_legacy_abac        = true 
+  remove_default_node_pool  = true
+  initial_node_count        = var.initial_node_count
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    }
+  
+resource "google_container_node_pool" "default" {
+  name       = "${var.name}-node-pool"
+  project    = var.project
+  location   = var.location
+  cluster    = google_container_cluster.default.name
+  node_count = 3
 
-  owners = ["099720109477"] # Canonical
-}
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
 
-  tags = {
-    Name = var.instance_name
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
