48c838dde137377ae29e2c24c3f4907790d39132
Jorg3Garci4 <154987426+Jorg3Garci4@users.noreply.github.com>
2023-12-27 19:42:23 -0500

    update provider

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,4 @@
 terraform {
-
   cloud {
     organization = "LATAM_AIRLINES"
 
@@ -7,4 +6,13 @@ terraform {
       name = "TESTER_JORGAR"
     }
   }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.77.0"
+    }
+  }
+
+  required_version = "~> 1.2"
 }
