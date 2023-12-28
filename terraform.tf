1bf00a7f79afcba4fa7ee7b8bbdd9c6de5acfbdc
Jorg3Garci4 <154987426+Jorg3Garci4@users.noreply.github.com>
2023-12-27 21:21:48 -0500

    otromas

--- terraform.tf
+++ terraform.tf
@@ -17,19 +17,5 @@ terraform {
   required_version = "~> 1.2"
 }
 
-provider "google" {
-  credentials = file(var.google_credentials_path)
-  project     = var.project
-  region      = var.region
-}
 
-resource "google_container_cluster" "my_cluster" {
-  name     = var.cluster_name
-  location = var.region
-
-  node_pool {
-    name       = "default-node-pool"
-    node_count = 1
-  }
-}
 
